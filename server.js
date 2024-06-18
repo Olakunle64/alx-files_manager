5198fcfb0bf46b2589ebe681f45c60cad69939e0
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-18 01:26:27 +0100

    correct the eslint error in the server.js file

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes/index';
 
-
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use('/', router);
 app.listen(port, () => {
-    console.log("Server running on port 5000");
+  console.log('Server running on port 5000');
 });
 
 export default app;
