a41476a58f5e5a454b51d46606f106ba4ef0ddf9
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-21 21:08:40 +0100

    use nodejs version 12.13

--- 0-test.js
+++ 0-test.js
@@ -1,15 +1,71 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import MongoClient from 'mongodb';
 
 chai.use(chaiHttp);
 
-describe('GET /status', () => {
-    it('GET /status exists', (done) => {
+// describe('GET /status', () => {
+//     it('GET /status exists', (done) => {
+//         chai.request('http://localhost:5000')
+//             .get('/status')
+//             .end((err, res) => {
+//                 chai.expect(err).to.be.null;
+//                 chai.expect(res).to.have.status(200);
+//                 done();
+//             });
+//     }).timeout(30000);
+// });
+
+
+// describe('GET /status', () => {
+//     it('GET /status exists', (done) => {
+//         chai.request('http://localhost:5000')
+//             .get('/status')
+//             .end((err, res) => {
+//                 chai.expect(err).to.be.null;
+//                 chai.expect(res).to.have.status(200);
+//                 const bodyJson = res.body;
+//                 chai.expect(bodyJson.redis).to.be.true;
+//                 chai.expect(bodyJson.db).to.be.true;
+//                 done();
+//             });
+//     }).timeout(30000);
+// });
+
+// chai.use(chaiHttp);
+
+describe('GET /stats', () => {
+    let testClientDb = null;
+
+    beforeEach(() => {
+        const dbInfo = {
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT || '27017',
+            database: process.env.DB_DATABASE || 'files_manager'
+        };
+        return new Promise((resolve) => {
+            MongoClient.connect(`mongodb://${dbInfo.host}:${dbInfo.port}/${dbInfo.database}`, async (err, client) => {
+                testClientDb = client.db(dbInfo.database);
+            
+                await testClientDb.collection('users').deleteMany({})
+
+                resolve();
+            }); 
+        });
+    });
+        
+    afterEach(() => {
+    });
+
+    it('GET /stats exists', (done) => {
         chai.request('http://localhost:5000')
-            .get('/status')
+            .get('/stats')
             .end((err, res) => {
                 chai.expect(err).to.be.null;
                 chai.expect(res).to.have.status(200);
+                const bodyJson = res.body;
+                chai.expect(bodyJson.users).to.equal(0);
+                chai.expect(bodyJson.files).to.equal(0);
                 done();
             });
     }).timeout(30000);
